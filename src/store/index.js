8c93af26cf8d6786e744b822b0885456e9106ccf
praveen-me <praveenkrsaini0@gmail.com>
2021-03-25 16:20:16 +0530

    Added online demo link to the README.md

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,10 @@ import { applyMiddleware, compose, createStore } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
-const composeEnhancer = process.env.NODE_ENV
-  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?? compose
-  : compose
+const composeEnhancer =
+  process.env.NODE_ENV === 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?? compose
+    : compose
 
 const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)))
 
